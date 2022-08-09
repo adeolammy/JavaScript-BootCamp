9ade4317ec757f0e2da878b9b7e815dc2d083d22
adeolammy <adeolammy2020@gmail.com>
2022-08-09 11:34:46 +0100

    first commit

---
+++ day_1/app.js
@@ -0,0 +1,253 @@
+
+// QUESTION  1   QUESTION  1   QUESTION  1  QUESTION  1  QUESTION  1  QUESTION  1 
+
+
+// create a var for jude and james. Trying to compare their BMI using a formular 
+// BMI = Mass / Height **2 or BMI = Mass / (Height * Height)
+// Mass = kg and Height = mts
+
+
+
+// ==================================================
+        // let jameHieght = 2.4, jamesMass = 88 ;
+        // let jamesBmi = jamesMass / jameHieght **2;
+
+
+        // let judeMass = 90, judeHeight = 1.5;
+        // const judeBmi = judeMass / (judeHeight * judeHeight)
+        // console.log(judeBmi + 'kg');
+
+
+// ==================================================
+    // let jameHieght = 2.9, jamesMass = 98;
+    // let jamesBmi = jamesMass / jameHieght **2;
+    // console.log(jamesBmi + 'kg');
+
+
+    // let judeHeight = 2.1, judeMass = 98;
+    // let judeBmi = judeMass / judeHeight;
+    // console.log(judeBmi + 'kg');
+
+    // const finalValue = judeBmi != jameHieght
+    // console.log(finalValue);
+// ==================================================
+
+// let jamesMass = 67 , jamesHeight = 2
+// jamesBmi =   jamesMass / jamesHeight **2
+// console.log(jamesBmi + 'kg');
+ 
+// let judeMass = 89, judeHeight = 2.4;
+// judeBmi = judeMass / (judeHeight * judeHeight)
+// console.log(judeBmi + 'kg');
+
+// const outputs = jamesBmi < judeBmi
+// console.log(outputs );
+
+// ==================================================
+// let jamesBmi = 92 / (1.5 * 1.5);
+// console.log(jamesBmi + 'kg');
+
+// let judeBmi = 102 / 1.9 **2
+// console.log(judeBmi + 'kg');
+
+// let diffBmi = judeBmi < jamesBmi;
+// console.log(diffBmi );
+
+
+// ==================================================
+// var judeMass = 82;
+// var judeHeight = 1.6;
+// var JudeBmi = judeMass / judeHeight **2;
+// console.log(JudeBmi + 'kg');
+
+
+// var jamesMass = 89;
+// var jamesHeight =  1.9;
+// var jamesBmi = jamesMass / (jamesHeight * jamesHeight);
+
+
+// var right  =  jamesBmi != JudeBmi;
+// console.log(right);
+
+
+// ==================================================
+
+
+// QUESTION 2    QUESTION 2    QUESTION 2    QUESTION 2    QUESTION 2    QUESTION 2    
+
+//(1) declare variable called "COUNTRY", "CONTINENT" & "POPULATION", and assign their values according 
+// to your own country (population in million )
+// (2)Log their values to the console
+
+
+// 1    1    1 1 1 1 1 1     1 1  1 1 1 1 1  
+
+// var country = 'Nigeria';
+// var continent = 'Africa';
+// var population = 212000000;
+// console.log('My country is '+ country, 'is in the continent of '+ continent,'it has over ' + population + ' in population');
+
+
+
+// 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+
+// var country = '';
+// var continent = '';
+// var population = '';
+
+// country = 'ghana';
+// continent = 'asia';
+// population = 23476789;
+
+// console.log('My country is '+ country, 'is in the continent of '+ continent,'it has over ' + population + ' in population');
+
+
+
+//  3 3 3 3 3 3 3 3 3 3 3 3 3    3   3   3   3   3   3   3       3   3
+// let country, continent, population = '';
+
+// country = 'lybia';
+// continent = 'Africa';
+// population = 9034433;
+
+// console.log('My country is '+ country, 'is in the continent of '+ continent,'it has over ' + population + ' in population');
+
+
+
+// QUESTION 4  QUESTION 4  QUESTION 4  QUESTION 4  QUESTION 4  QUESTION 4  QUESTION 4  
+
+//  (1) declear a vairable called isIsland and set its value according to your country.
+// the variaible should hold a Boolean value. Also declare a variable LANNGUAGE. But dont assign it any value yet.
+//  (2)  Log the types of 'isIsland' , 'Population', 'Country', and 'Language'
+
+
+// ====================================================================
+
+// let myCountry = 'spain';
+// let isIsland = myCountry == 'spain';
+// console.log(isIsland );
+
+
+// let language ;
+// let population = 5678907654;
+// let country = 'japan';
+
+// console.log(typeof(isIsland), typeof(language), typeof(population), typeof(country) );
+
+
+
+
+
+
+
+
+
+
+
+
+    
+// ====================================================================
+// let isIsland = 'kano Nigeria';
+// const isIsland1 = true;
+// const country = 'Nigeria';
+// let population = 765445;
+// let Language;
+// console.log('typeof isIsland is ', typeof(isIsland));
+// console.log('typeof isIsland is ', typeof(isIsland1));
+// console.log('typeof population is ', typeof(population));
+// console.log('typeof country is ', typeof(country));
+// console.log('typeof Language is ', typeof(Language));
+// ====================================================================
+
+// const isIsland = 'lagos Nigeria';
+// const isIsland1 = false;
+// const country = 'Nigeria';
+// let population = 2345678976543;
+// let Language = '';
+// console.log('typeof isIsland is ', typeof(isIsland));
+// console.log('typeof isIsland is ', typeof(isIsland1));
+// console.log('typeof population is ', typeof(population));
+// console.log('typeof country is ', typeof(country));
+// console.log('typeof Language is ', typeof(Language));
+
+// ====================================================================
+
+
+// let myIsland = 'England' 
+// let isIsland = myIsland == 'England';
+// let Language;
+// let population = 3456;
+// let country = 'Mali'
+// console.log('isIsland' + ' is a ', typeof(isIsland));
+// console.log('Language' + ' is a ',typeof(Language));
+// console.log('population' + ' is a ', typeof(population) );
+// console.log('country' + ' is a ', typeof(country) );
+
+
+
+
+// QUESTION 5  QUESTION 5  QUESTION 5  QUESTION 5  QUESTION 5  QUESTION 5  QUESTION 5 
+
+// let Language = 'yoruba';
+
+
+//  QUESTION 6  QUESTION 6 QUESTION 6 QUESTION 6 QUESTION 6 QUESTION 6
+
+// (1) if your country split in half and each half would contian half the population,
+//      then how many people would live in each half
+// (2) increase the polulation of your country by 1 and log the result to the console
+// (3)  finland has a population of 6 million. Does your couintry have more people than Finland?
+// (4)  The average population of a country is 33million people.  Does your country have less people than the average country?
+// (5)  Based on the variable you created. create a new variable 'description' which contains a strings
+//  with this format: 'Potugal is in Europe, and its 11 million people speak portuguese'
+
+
+
+// let nigeriaPopulation = 250000000;
+// let nigeriasHalf = nigeriaPopulation / 2; // Question 1   Question 1  Question 1
+// console.log(nigeriasHalf);
+
+// nigeriaPopulation += 1 // Question 2   Question 2  Question 2
+// console.log(nigeriaPopulation);
+
+// let finlandPopulation = 6000000;
+// const finalOutput = nigeriaPopulation > finlandPopulation; // Question 3   Question 3  Question 3
+// console.log(finalOutput);
+
+
+// let averagePopulation = 33000000;
+// const ansForAverage = averagePopulation > nigeriasHalf  // Question 4   Question 4  Question 4
+// console.log(ansForAverage);
+
+
+// var description = 'Potugal is in Europe, and its 11 million people speak portuguese'
+// console.log(description);   // Question 5   Question 5  Question 5
+
+
+
+
+// (1)
+
+// let NigeriaPopulationHalf = 1233434343 / 2;
+// console.log(NigeriaPopulationHalf);
+
+// // (2)
+//  NigeriaPopulationHalf += 1;
+// console.log(NigeriaPopulationHalf);
+
+// // (3)
+// let Nigeria = 1233434343
+// const finland =  6000000;
+// let isFinlandMore = finland > Nigeria;
+// console.log(isFinlandMore);
+
+// // (4)
+
+// const averagePop = 33000000;
+// let moThanAverage =  NigeriaPopulationHalf > averagePop;
+// console.log(moThanAverage);
+
+// // (5)
+
+// const description = 'Potugal is in Europe, and its 11 million people speak portuguese';
+// console.log(description);
