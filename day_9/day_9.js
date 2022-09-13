91c61fbbd5aec47d0da031d6dba196a1d72598da
adeolammy <adeolammy2020@gmail.com>
2022-09-13 07:20:58 +0100

    another commit

--- day_9/day_9.js
+++ day_9/day_9.js
@@ -1,5 +1,22 @@
+// BOOK CONSTRUCTOR
+
 function Book(title, author, isbn) {
     this.title = title;
     this.author  = author;
     this.isbn = isbn;
 }
+
+// UI CONSTRUCTOR
+    function UI() {
+        
+    }
+// ADDING EVENT LISTERNER
+document.getElementById('book-form').addEventListener('submit', function (e) {
+    const title = document.getElementById('title').value,    
+          author = document.getElementById('author').value,   
+          isbn = document.getElementById('isbn').value 
+        //   console.log(title, author, isbn);   
+          e.preventDefault();
+          const book = new Book(title,author,isbn);
+          console.log(book);
+})
