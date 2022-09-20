eac5fc5fe0d4f00b9d6161b342bf01644e1b58d3
adeolammy <adeolammy2020@gmail.com>
2022-09-20 08:49:18 +0100

    more exercises

--- day_10/day_10.js
+++ day_10/day_10.js
@@ -1,5 +1,5 @@
-const btn = document.getElementById('btn')
-const output = document.getElementById('output')
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
 
 btn.addEventListener('click', ()=>{
 
@@ -7,16 +7,16 @@ btn.addEventListener('click', ()=>{
 
     xhr.open('GET', 'data.txt', true);
 
-    xhr.onload = function () {
-        if (this.status === 200) {
-            output.innerHTML = this.responseText
-            // console.log(this.responseText);
+    // xhr.onload = function () {
+    //     if (this.status === 200) {
+    //         output.innerHTML = this.responseText;
+    //     }
+    // }
+
+    xhr.onreadystatechange = function () {
+        if (this.status === 200 || this.onreadystatechange ===4) {
+            output.innerHTML = `<p style="background-color: lightblue;">${this.responseText}</p>`
         }
     }
     xhr.send()
 })
-
-// 1 create xhr object
-// 2 OPEN the xhr with 3 para (Get, d data, boolean)
-// 3 xhr.onload = function{ if( this.status ===200) this.responseText}
-// 4 xhr.send()
