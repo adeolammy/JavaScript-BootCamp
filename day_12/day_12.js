eae233b22bc008f34a7a37a8c331541242affd07
adeolammy <adeolammy2020@gmail.com>
2022-09-24 06:41:55 +0100

    fetching from a JSON file

--- day_12/day_12.js
+++ day_12/day_12.js
@@ -1,7 +1,9 @@
-const btn1 = document.getElementById('btn1');
+const getTextBtn = document.getElementById('btn1');
+const getJsonBtn = document.getElementById('btn2');
+const getApiBtn = document.getElementById('btn3');
 const output = document.getElementById('output');
 
-btn1.addEventListener('click', ()=>{
+getTextBtn.addEventListener('click', ()=>{
   fetch('text.txt')
   .then((res)=>{ 
     return res.text();
@@ -11,7 +13,30 @@ btn1.addEventListener('click', ()=>{
     // console.log(data);
   })
   .then( function(err) {
-    err = `<span style="background-color: yellow;">pass in the right data</span>`
+    // err = `<span style="background-color: yellow;">pass in the right data</span>`
     output.innerHTML += err;
   })
+})
+
+
+getJsonBtn.addEventListener('click', ()=>{
+    fetch('datas.json')
+    .then((res)=>{
+      return  res.json();
+    })
+
+    .then((data)=>{
+        data.map(result => {
+            output.innerHTML += `<p>${result.id}</p>
+                                <p>${result.names}</p>
+                                <p>${result.Address}</p>
+                                <p>${result.phone}</p>
+                                <p>${result.Company}</p>
+            `
+        });
+    })
+    .catch((err)=>{
+        // console.log(err);
+        output.innerHTML += err;
+    })
 })
